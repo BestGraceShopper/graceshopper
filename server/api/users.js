4cadf92e05a8ab4ea8950bfc421e1d401df4d856
Jonathan Ciccarella <JonCiccarella@gmail.com>
2019-01-08 16:54:33 -0500

    api route for user id

--- server/api/users.js
+++ server/api/users.js
@@ -15,3 +15,21 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const users = await User.findOne({
+      // explicitly select only the id and email fields - even though
+      // users' passwords are encrypted, it won't help if we just
+      // send everything to anyone who asks!
+      where: {
+        id: req.params.id,
+      },
+      attributes: ['id', 'email', 'address', 'phone'],
+    })
+    res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
