343535c128204647c338b567ec91480c860b1cf3
calvin ho <calvinho1214@gmail.com>
2019-01-08 15:23:36 -0500

    added Navbar with home and cart button

--- client/app.js
+++ client/app.js
@@ -1,15 +1,27 @@
-import React from 'react'
-
-import {Navbar} from './components'
+import React, { Component } from 'react'
+import { Input, Menu, Segment} from 'semantic-ui-react'
 import Routes from './routes'
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
-}
+export default class App extends Component {
+  state = {activeItem: 'home'}
 
-export default App
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render(){
+    const { activeItem } = this.state
+
+    return(
+      <div>
+        <Menu pointing>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item position='right' name='cart' active={activeItem === 'cart'} onClick={this.handleItemClick} />
+          
+        </Menu>
+        <Segment>
+          <Routes />
+        </Segment>
+      </div>
+
+    )
+  }
+}
