553cff92ab4d85527a2239962a0ed674a254e9b1
Justin <mintzer.justin@gmail.com>
2019-01-09 11:11:50 -0500

    Navbar buttons work and cart page started

---
+++ client/components/Cart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Card, Icon, Image, Button} from 'semantic-ui-react'
+
+const Cart = props => (
+  <div>
+      This will be the cart
+  </div>
+)
+
+export default Cart
