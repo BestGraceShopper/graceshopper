f266f0ae2e1954cf9290c912e0bcf22db9e62a5a
Justin <mintzer.justin@gmail.com>
2019-01-10 18:31:33 -0500

    Revert this to previous CartItem

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,39 +1,35 @@
 import React from 'react'
-import { Item, Image, Button, Icon, Dropdown, Menu } from 'semantic-ui-react'
+import { Item } from 'semantic-ui-react'
+import CartItem from './CartItem'
+import { connect } from 'react-redux'
+import { getProduct } from '../store/reducers/product'
 
-const options = () => {
-  let arr = []
-  for (let i = 1; i <= 10; i++) {
-    arr.push({ key: i, text: i, value: i })
+class Cart extends React.Component {
+  render() {
+    const { cart } = this.props
+    return (
+      <div>
+        <Item.Group>
+          {cart.map((item, idx) => <CartItem product={item} key={idx} />)}
+        </Item.Group>
+      </div>
+    )
   }
-  return arr
 }
 
-const Cart = props => (
-  <Item.Group>
-    <Item>
-      <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-      <Item.Content>
-        <Item.Header>Item Name</Item.Header>
-        <Item.Meta>$3050.00</Item.Meta>
-        <Item.Description>
-          <span>Description........</span>
-        </Item.Description>
-        <Item.Extra>
-          <a href="">Remove from Cart</a>
-          <Menu compact floated="right">
-            <Dropdown
-              labeled={true}
-              defaultValue={1}
-              options={options()}
-              simple
-              item
-            />
-          </Menu>
-        </Item.Extra>
-      </Item.Content>
-    </Item>
-  </Item.Group>
-)
+const mapState = state => {
+  return {
+    cart: state.product.cart
+  }
+}
 
-export default Cart
+const mapDispatch = dispatch => {
+  return {
+    fetchSingleProduct: id => {
+      dispatch(getProduct(id))
+    }
+  }
+}
+
+const connectedCart = connect(mapState, mapDispatch)(Cart)
+export default connectedCart
