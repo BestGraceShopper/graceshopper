b4db3ed22c0c17cedbe842857fadc0afd21e5f9b
Jonathan Ciccarella <JonCiccarella@gmail.com>
2019-01-15 16:59:54 -0500

    added hero gif for homepage

---
+++ client/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Image } from 'semantic-ui-react'
+
+const ImageExampleFluid = () => (
+  <Image
+    src="https://i.pinimg.com/originals/2e/aa/94/2eaa9476c6cbe9624a840ed279ed2b51.gif"
+    fluid
+  />
+)
+
+export default ImageExampleFluid
