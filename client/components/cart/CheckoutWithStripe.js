d17b131093328bafb978ab46762a9dad17e3d326
Jonathan Ciccarella <JonCiccarella@gmail.com>
2019-01-15 15:43:36 -0500

    Stripe implemented. Only blocker is adding transaction to db

--- client/components/cart/CheckoutWithStripe.js
+++ client/components/cart/CheckoutWithStripe.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { CardElement, injectStripe } from 'react-stripe-elements'
+import { Message } from 'semantic-ui-react'
+import { purchaseOrder } from '../../store/reducers/cart'
 
-class CheckoutForm extends Component {
+class CheckoutWithStripe extends Component {
   constructor(props) {
     super(props)
     this.state = { complete: false }
     this.submit = this.submit.bind(this)
+
+    this.successCallback = this.successCallback.bind(this)
+  }
+
+  successCallback() {
+    this.setState({
+      complete: true
+    })
   }
 
   async submit(ev) {
     ev.preventDefault()
-    // User clicked submit
-    let { token } = await this.props.stripe.createToken({ name: 'Name' })
-    let response = await fetch('/charge', {
-      method: 'POST',
-      headers: { 'Content-Type': 'text/plain' },
-      body: token.id
-    })
-
-    if (response.ok) console.log('Purchase Complete!')
+    // User clicked submit (if we had more time, make this a thunk in our reducers)
+    try {
+      let { token } = await this.props.stripe.createToken({ name: 'Name' })
+      let response = await fetch(
+        `/api/users/${this.props.user.id}/cart/charge`,
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'text/plain' },
+          body: token.id
+        }
+      )
+      if (response.ok) {
+        this.successCallback()
+        // this.props.isLoggedIn
+        //   ? this.props.makePurchaseOrder(this.props.user.id, this.props.cart)
+        //   : this.props.makePurchaseOrder('guest', this.props.cart)
+      }
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   render() {
@@ -30,9 +52,31 @@ class CheckoutForm extends Component {
         <button type="submit" onClick={this.submit}>
           Complete Purchase
         </button>
+        {this.state.complete ? (
+          <Message
+            success={this.state.complete}
+            header="All Set!"
+            content="Your order is on the way.  Check your email for shipping details"
+          />
+        ) : null}
       </div>
     )
   }
 }
 
-export default injectStripe(CheckoutForm)
+const mapStateToProps = state => {
+  return {
+    user: state.user.user,
+    isLoggedIn: !!state.user.user.id
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    makePurchaseOrder: (id, cart) => dispatch(purchaseOrder(id, cart))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  injectStripe(CheckoutWithStripe)
+)
--- client/components/cart/CustomerForm.js
+++ client/components/cart/CustomerForm.js
@@ -1,31 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Button, Form, Message } from 'semantic-ui-react'
-import { purchaseOrder } from '../../store/reducers/cart'
+import { Form } from 'semantic-ui-react'
+
 class FormSuccess extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      complete: false
-    }
-    this.successCallback = this.successCallback.bind(this)
-    this.handlePurchaseClick = this.handlePurchaseClick.bind(this)
-  }
-
-  successCallback() {
-    this.setState({
-      complete: true
-    })
-  }
-
-  handlePurchaseClick() {
-    this.successCallback()
-    this.props.isLoggedIn
-      ? this.props.makePurchaseOrder(this.props.user.id, this.props.cart)
-      : this.props.makePurchaseOrder('guest', this.props.cart)
-    // then action to actually make the purchase using Stripe
-  }
-
   render() {
     const { isLoggedIn, user } = this.props
     return (
@@ -46,15 +23,6 @@ class FormSuccess extends Component {
             isLoggedIn ? user.address || '1234 Main street' : '1234 Main street'
           }
         />
-        {this.state.complete ? (
-          <Message
-            success={this.state.complete}
-            header="All Set!"
-            content="Your order is on the way.  Check your email for shipping details"
-          />
-        ) : null}
-
-        <Button onClick={this.handlePurchaseClick}>Purchase</Button>
       </Form>
     )
   }
@@ -62,18 +30,12 @@ class FormSuccess extends Component {
 
 const mapStateToProps = state => {
   return {
-    isLoggedIn: !!state.user.user.id,
     user: state.user.user,
+    isLoggedIn: !!state.user.user.id,
     products: state.product.products,
     cart: state.cart.cart,
     orderSummary: state.product.orderSummary
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    makePurchaseOrder: (id, cart) => dispatch(purchaseOrder(id, cart))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(FormSuccess)
+export default connect(mapStateToProps)(FormSuccess)
