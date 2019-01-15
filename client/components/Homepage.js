2b8dee896b075dede61b686cbafb510c7e55b9b1
Jonathan Ciccarella <JonCiccarella@gmail.com>
2019-01-15 17:41:40 -0500

    more updtes to homepage

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Image } from 'semantic-ui-react'
 
 const ImageExampleFluid = () => (
-  <Image
-    src="https://i.pinimg.com/originals/2e/aa/94/2eaa9476c6cbe9624a840ed279ed2b51.gif"
-    fluid
-  />
+  <div className="homepage">
+    <Image
+      src="https://i.pinimg.com/originals/2e/aa/94/2eaa9476c6cbe9624a840ed279ed2b51.gif"
+      fluid
+    />
+  </div>
 )
 
 export default ImageExampleFluid
