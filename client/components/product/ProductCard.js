58d371b981b7deeaf439c1a2800e604a796184e7
calvin ho <calvinho1214@gmail.com>
2019-01-15 00:51:25 -0500

    was missing functionality to the picture links in product card

--- client/components/product/ProductCard.js
+++ client/components/product/ProductCard.js
@@ -4,7 +4,14 @@ import { Card, Icon, Image, Button } from 'semantic-ui-react'
 
 const ProductCard = props => (
   <Card>
-    <Image centered height={150} width={150} src={props.product.imageUrl} />
+    <Image
+      as={Link}
+      to={`/products/${props.product.id}`}
+      centered
+      height={150}
+      width={150}
+      src={props.product.imageUrl}
+    />
     <Card.Content>
       <Card.Header>{props.product.name}</Card.Header>
       <Card.Meta>
