c37e068192224c7090e7ad8db59d403d3025b1a6
Jonathan Ciccarella <JonCiccarella@gmail.com>
2019-01-15 19:47:33 -0500

    simple image and store name added for homepage

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import { Image } from 'semantic-ui-react'
 
-const ImageExampleFluid = () => (
+const divStyle = {
+  padding: '40px 40px',
+  backgroundColor: 'pink',
+  textAlign: 'center',
+  fontSize: '80px'
+}
+const pStyle = {
+  backgroundColor: 'black'
+}
+
+const homepage = () => (
   <div className="homepage">
-    <Image
-      src="https://i.pinimg.com/originals/2e/aa/94/2eaa9476c6cbe9624a840ed279ed2b51.gif"
-      fluid
-    />
+    <div style={divStyle}>
+      <p>Little Gracie</p>
+    </div>
+    <div>
+      <Image
+        src="https://i.pinimg.com/originals/2e/aa/94/2eaa9476c6cbe9624a840ed279ed2b51.gif"
+        centered
+      />
+    </div>
+    <div style={divStyle} />
   </div>
 )
 
-export default ImageExampleFluid
+export default homepage
